eacb3e58cfd17cf4946c29a5c2bcba8ce4462349
vecpeng <841298391@qq.com>
2022-05-24 17:02:34 +0800

    feat: css解析

--- test_reg.js
+++
@@ -1,7 +0,0 @@
-// 匹配以字母数字或者@开头
-const BARE_IMPORT_RE = /^[\w@][^:]/;
-
-console.log(BARE_IMPORT_RE.test('react'));
-console.log(BARE_IMPORT_RE.test('react-dom'));
-console.log(BARE_IMPORT_RE.test('react@test'));
-console.log(BARE_IMPORT_RE.test('re:react@test.js'));
